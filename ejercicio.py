37843251b7140168499cfbc3d03b1e2c7c3eb493
itspablito <juan.rodriguez.g@correounivalle.edu.co>
2023-09-19 14:23:17 -0500

    ejercicio 1 realizado

---
+++ ejercicio.py
@@ -0,0 +1,39 @@
+
+
+
+
+def areat(base,altura,profundidad):
+    area = (base*altura)*profundidad
+    return area
+
+
+
+
+if __name__ == "__main__":
+    pf1= areat(5,5,5)
+    pf2= areat(5,5,10)
+    pf3= areat(5,5,100)
+    print(pf1)
+    print(pf2)
+    print(pf3)
+
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
